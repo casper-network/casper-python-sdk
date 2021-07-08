74ac7f2505c68ae6f809c878d204b3ef985eb464
Mark A. Greenslade <mark@casperlabs.io>
2021-07-08 22:56:51 +0200

    1. Minor change to byte array encoder.

--- pycspr/codec/byte_array/__init__.py
+++ pycspr/codec/byte_array/__init__.py
@@ -1,1 +1,16 @@
-from pycspr.codec.byte_array.encoder import encode
+import typing
+
+from pycspr.codec.byte_array.encoder_cl import encode as encoder_cl
+from pycspr.codec.byte_array.encoder_deploy import encode as encoder_deploy
+from pycspr.types import CLValue
+
+
+
+def encode(value) -> typing.List[int]:
+    """Encodes a domain value as an array of bytes.
+    
+    """
+    if type(value) == CLValue:       
+        return encoder_cl(value) 
+    else:
+        return encoder_deploy(value) 
