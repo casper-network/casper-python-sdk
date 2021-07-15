db308be901f488a21e1f021a0c403b16e521555f
Mark A. Greenslade <mark@casperlabs.io>
2021-07-15 10:50:30 +0200

    1. Updated json decoder to support full roundtrip.

--- tests/test_deploy_lifecycle.py
+++ tests/test_deploy_lifecycle.py
@@ -2,37 +2,37 @@ import random
 import tempfile
 
 
-# def test_deploy_is_unapproved_when_instantiated(FACTORY, deploy_params, cp2):
-#     deploy = _create_deploy(FACTORY, deploy_params, cp2)
-#     assert len(deploy.approvals) == 0
+def test_deploy_is_unapproved_when_instantiated(FACTORY, deploy_params, cp2):
+    deploy = _create_deploy(FACTORY, deploy_params, cp2)
+    assert len(deploy.approvals) == 0
 
 
-# def test_deploy_can_be_approved(FACTORY, deploy_params, cp1, cp2):
-#     deploy = _create_deploy(FACTORY, deploy_params, cp2)
-#     deploy.set_approval(cp1)
-#     assert len(deploy.approvals) == 1
-#     assert deploy.approvals[0].signer == cp1.account_key
+def test_deploy_can_be_approved(FACTORY, deploy_params, cp1, cp2):
+    deploy = _create_deploy(FACTORY, deploy_params, cp2)
+    deploy.set_approval(cp1)
+    assert len(deploy.approvals) == 1
+    assert deploy.approvals[0].signer == cp1.account_key
 
 
-# def test_deploy_can_be_approved_by_multiple_parties(FACTORY, deploy_params, cp1, cp2):
-#     deploy = _create_deploy(FACTORY, deploy_params, cp2)
-#     deploy.set_approval(cp1)
-#     deploy.set_approval(cp2)
-#     assert len(deploy.approvals) == 2
+def test_deploy_can_be_approved_by_multiple_parties(FACTORY, deploy_params, cp1, cp2):
+    deploy = _create_deploy(FACTORY, deploy_params, cp2)
+    deploy.set_approval(cp1)
+    deploy.set_approval(cp2)
+    assert len(deploy.approvals) == 2
 
 
-# def test_deploy_approvals_are_deduplicated(FACTORY, deploy_params, cp1, cp2):
-#     deploy = _create_deploy(FACTORY, deploy_params, cp2)
-#     for _ in range(10):
-#         deploy.set_approval(cp1)
-#         deploy.set_approval(cp2)
-#     assert len(deploy.approvals) == 2
+def test_deploy_approvals_are_deduplicated(FACTORY, deploy_params, cp1, cp2):
+    deploy = _create_deploy(FACTORY, deploy_params, cp2)
+    for _ in range(10):
+        deploy.set_approval(cp1)
+        deploy.set_approval(cp2)
+    assert len(deploy.approvals) == 2
 
 
-# def test_can_write_to_fs(LIB, FACTORY, deploy_params, cp1, cp2):
-#     deploy = _create_deploy(FACTORY, deploy_params, cp2)
-#     with tempfile.TemporaryFile() as fp:
-#         LIB.write_deploy(deploy, fp)        
+def test_can_write_to_fs(LIB, FACTORY, deploy_params, cp1, cp2):
+    deploy = _create_deploy(FACTORY, deploy_params, cp2)
+    with tempfile.TemporaryFile() as fp:
+        LIB.write_deploy(deploy, fp)        
 
 
 def test_can_read_from_fs(LIB, FACTORY, deploy_params, cp1, cp2):
