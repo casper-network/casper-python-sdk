6895e995a980e2d2ab9c800e8ae6fa5b7e1fb597
Mark A. Greenslade <mark@casperlabs.io>
2021-07-19 15:50:26 +0200

    1. Adding how-to-transfer workflow.

--- docs/how_to_transfer.py
+++ docs/how_to_transfer.py
@@ -50,6 +50,41 @@ _ARGS.add_argument(
     type=str,
     )
 
+# CLI argument: host address of target node - defaults to NCTL node 1.
+_ARGS.add_argument(
+    "--node-host",
+    default="localhost",
+    dest="node_host",
+    help="Host address of target node.",
+    type=str,
+    )
+
+# CLI argument: Node API REST port - defaults to 14101 @ NCTL node 1.
+_ARGS.add_argument(
+    "--node-port-rest",
+    default=14101,
+    dest="node_port_rest",
+    help="Node API REST port.  Typically 8888 on most nodes.",
+    type=int,
+    )
+
+# CLI argument: Node API JSON-RPC port - defaults to 11101 @ NCTL node 1.
+_ARGS.add_argument(
+    "--node-port-rpc",
+    default=11101,
+    dest="node_port_rpc",
+    help="Node API JSON-RPC port.  Typically 7777 on most nodes.",
+    type=int,
+    )
+
+# CLI argument: Node API SSE port - defaults to 18101 @ NCTL node 1.
+_ARGS.add_argument(
+    "--node-port-sse",
+    default=18101,
+    dest="node_port_sse",
+    help="Node API SSE port.  Typically 9999 on most nodes.",
+    type=int,
+    )
 
 
 def _main(args: argparse.Namespace):
@@ -75,10 +110,10 @@ def _get_client(args: argparse.Namespace) -> pycspr.NodeClient:
     """
     # Set connection.
     connection = pycspr.NodeConnectionInfo(
-        host="localhost",
-        port_rest=14101,
-        port_rpc=11101,
-        port_sse=18101
+        host=args.node_host,
+        port_rest=args.node_port_rest,
+        port_rpc=args.node_port_rpc,
+        port_sse=args.node_port_sse
     )
 
     return pycspr.NodeClient(connection)
