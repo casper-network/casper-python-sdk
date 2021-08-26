39a2e2035e7ebf5c4b0230c65b8a8f9ab1ae4055
Mark A. Greenslade <mark@casperlabs.io>
2021-08-26 16:18:23 +0200

    1. Moved endpoint constants to dedicated file.  2. Updated api sub-package in light of new endpoints.

--- pycspr/api/constants.py
+++ pycspr/api/constants.py
@@ -1,4 +1,4 @@
-# RPC endpoints.
+# Node RPC endpoints.
 RPC_ACCOUNT_PUT_DEPLOY = "account_put_deploy"
 RPC_CHAIN_GET_BLOCK = "chain_get_block"
 RPC_CHAIN_GET_BLOCK_TRANSFERS = "chain_get_block_transfers"
@@ -15,8 +15,40 @@ RPC_STATE_GET_DICTIONARY_ITEM = "state_get_dictionary_item"
 RPC_STATE_GET_ITEM = "state_get_item"
 RPC_STATE_QUERY_GLOBAL_STATE = "query_global_state"
 
-# REST endpoints.
+RPC_ENDPOINTS: set = {
+    RPC_ACCOUNT_PUT_DEPLOY,
+    RPC_CHAIN_GET_BLOCK,
+    RPC_CHAIN_GET_BLOCK_TRANSFERS,
+    RPC_CHAIN_GET_ERA_INFO_BY_SWITCH_BLOCK,
+    RPC_CHAIN_GET_STATE_ROOT_HASH,
+    RPC_DISCOVER,
+    RPC_INFO_GET_DEPLOY,
+    RPC_INFO_GET_PEERS,
+    RPC_INFO_GET_STATUS,
+    RPC_STATE_GET_ACCOUNT_INFO,
+    RPC_STATE_GET_AUCTION_INFO,
+    RPC_STATE_GET_BALANCE,
+    RPC_STATE_GET_DICTIONARY_ITEM,
+    RPC_STATE_GET_ITEM,
+    RPC_STATE_QUERY_GLOBAL_STATE,
+    }
+
+# Node REST endpoints.
 REST_GET_METRICS = "metrics"
+REST_GET_STATUS = "status"
 
+REST_ENDPOINTS: set = {
+    REST_GET_METRICS,
+    REST_GET_STATUS,
+    }
 
-# SSE endpoints/channels.
+# Node SSE endpoints/channels.
+SSE_DEPLOYS = "deploys"
+SSE_MAIN = "main"
+SSE_SIGS = "sigs"
+
+SSE_ENDPOINTS: set = {
+    SSE_DEPLOYS,
+    SSE_MAIN,
+    SSE_SIGS,
+}
