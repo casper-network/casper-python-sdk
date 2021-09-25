7a73edb2a7c0d77c9797ae5997e7f99aa797d585
Mark A. Greenslade <mark@casperlabs.io>
2021-09-25 11:50:00 +0200

    1. Version bump.

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -107,7 +107,7 @@ _INTERFACE_OF_FACTORY = {
 
 
 def test_version_of_library():
-    assert pycspr.__version__ == "0.8.3"
+    assert pycspr.__version__ == "0.8.4"
 
 
 def test_exports_of_library():
