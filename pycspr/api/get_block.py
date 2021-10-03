d7f2be557d4fccd0843c3d3cb89f03df495718a4
Christof <christof@myway.de>
2021-10-03 11:02:34 +0200

    PEP code style conventions

--- pycspr/api/get_block.py
+++ pycspr/api/get_block.py
@@ -4,17 +4,15 @@ from pycspr.api import constants
 from pycspr.client import NodeConnectionInfo
 
 
+def execute(node: NodeConnectionInfo,
+            block_id: typing.Union[None, bytes, str, int] = None
+            ) -> dict:
+    """
+        Returns on-chain block information.
 
-def execute(
-    node: NodeConnectionInfo,
-    block_id: typing.Union[None, bytes, str, int] = None
-    ) -> dict:
-    """Returns on-chain block information.
-
-    :param node: Information required to connect to a node.
-    :param block_id: Identifier of a finalised block.
-    :returns: On-chain block information.
-
+        :param node: Information required to connect to a node.
+        :param block_id: Identifier of a finalised block.
+        :returns: On-chain block information.
     """
     params = get_params(block_id)
     response = node.get_response(constants.RPC_CHAIN_GET_BLOCK, params)
@@ -23,25 +21,24 @@ def execute(
 
 
 def get_params(block_id: typing.Union[None, str, int] = None) -> dict:
-    """Returns JSON-RPC API request parameters.
+    """
+        Returns JSON-RPC API request parameters.
 
-    :param block_id: Identifier of a finalised block.
-    :returns: Parameters to be passed to JSON-RPC API.
-
+        :param block_id: Identifier of a finalised block.
+        :returns: Parameters to be passed to JSON-RPC API.
     """
     if isinstance(block_id, type(None)):
         return None
-
     elif isinstance(block_id, (bytes, str)):
         return {
-            "block_identifier":{
-                "Hash": block_id.hex() if isinstance(block_id, bytes) else block_id
-            }            
+            "block_identifier": {
+                "Hash": block_id.hex()
+                if isinstance(block_id, bytes) else block_id
+            }
         }
-
     elif isinstance(block_id, int):
         return {
-            "block_identifier":{
+            "block_identifier": {
                 "Height": block_id
-            }            
+            }
         }
