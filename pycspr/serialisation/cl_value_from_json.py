cde1c31f22f303390077285a5e5b56c79f731224
Mark A. Greenslade <mark@casperlabs.io>
2021-11-22 20:30:19 +0100

    1. Corrected cl value serialisation error.

--- pycspr/serialisation/cl_value_from_json.py
+++ pycspr/serialisation/cl_value_from_json.py
@@ -4,14 +4,8 @@ from pycspr.serialisation.cl_value_from_bytes import decode as cl_value_from_byt
 
 def decode(encoded: dict):
     assert "cl_type" in encoded and "bytes" in encoded
+    as_bytes = \
+        bytes.fromhex(encoded["bytes"]) if isinstance(encoded["bytes"], str) else \
+        encoded["bytes"]
 
-    if isinstance(encoded["bytes"], str):
-        return cl_value_from_bytes(
-            bytes.fromhex(encoded["bytes"]),
-            cl_type_from_json(encoded["cl_type"])
-            )
-    else:
-        return cl_value_from_bytes(
-            encoded["bytes"],
-            cl_type_from_json(encoded["cl_type"])
-            )
+    return cl_value_from_bytes(as_bytes, cl_type_from_json(encoded["cl_type"]) )
