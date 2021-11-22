b6a99d63340b3642570ef20c0b1aea63cb41068a
Mark A. Greenslade <mark@casperlabs.io>
2021-11-22 21:43:18 +0100

    1. Updating unit tests.

--- tests/test_api_account_put_deploy.py
+++
@@ -1,6 +0,0 @@
-def test_can_send_a_deploy(CLIENT, a_deploy):
-    def _assert(response):
-        assert isinstance(response, str)
-        assert a_deploy.hash == bytes.fromhex(response)
-
-    _assert(CLIENT.send_deploy(a_deploy))
