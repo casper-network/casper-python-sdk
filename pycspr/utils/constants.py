196cbd6a9af4087f3f32746b9541a5a5f4173129
Mark A. Greenslade <mark@casperlabs.io>
2021-11-22 22:49:57 +0100

    1. Updated constants.

--- pycspr/utils/constants.py
+++ pycspr/utils/constants.py
@@ -1,24 +1,3 @@
-import collections
-
-
-# Data structure representing numerical constraints.
-NumericConstraints = collections.namedtuple("NumericConstraints", ["LENGTH", "MIN", "MAX"])
-
-# Default number of motes to pay for standard payments.
-STANDARD_PAYMENT_FOR_NATIVE_TRANSFERS = int(1e8)
-
-# Default number of motes to pay for standard delegation.
-STANDARD_PAYMENT_FOR_DELEGATION = int(3e9)
-
-# Default number of motes to pay for standard delegation withdrawal.
-STANDARD_PAYMENT_FOR_DELEGATION_WITHDRAWAL = int(3e9)
-
-# Default number of motes to pay for standard auction bid.
-STANDARD_PAYMENT_FOR_AUCTION_BID = int(3e9)
-
-# Default number of motes to pay for standard auction bid withdrawal.
-STANDARD_PAYMENT_FOR_AUCTION_BID_WITHDRAWAL = int(3e9)
-
 # Default deploy time to live.
 DEFAULT_DEPLOY_TTL = "30m"
 
@@ -30,3 +9,21 @@ DEPLOY_TTL_MS_MAX = 1000 * 60 * 60 * 24
 
 # Maximum value of a transfer ID.
 MAX_TRANSFER_ID = (2 ** 63) - 1
+
+# Minimum amount in motes of a wasmless transfer.
+MIN_TRANSFER_AMOUNT_MOTES = 2_500_000_000
+
+# Default number of motes to pay for standard payments.
+STANDARD_PAYMENT_FOR_NATIVE_TRANSFERS = int(1e8)
+
+# Default number of motes to pay for standard delegation.
+STANDARD_PAYMENT_FOR_DELEGATION = int(5e9)
+
+# Default number of motes to pay for standard delegation withdrawal.
+STANDARD_PAYMENT_FOR_DELEGATION_WITHDRAWAL = int(5e9)
+
+# Default number of motes to pay for standard auction bid.
+STANDARD_PAYMENT_FOR_AUCTION_BID = int(5e9)
+
+# Default number of motes to pay for standard auction bid withdrawal.
+STANDARD_PAYMENT_FOR_AUCTION_BID_WITHDRAWAL = int(5e9)
