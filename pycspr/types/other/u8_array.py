d39668f5a9dc559f87e4c2d29c47660fe0696e28
Mark A. Greenslade <mark@casperlabs.io>
2021-11-22 14:36:20 +0100

    1. Removed obsolete domain type serialisation code. 2. Added deploy to bytes support.

--- pycspr/types/other/u8_array.py
+++ pycspr/types/other/u8_array.py
@@ -5,17 +5,3 @@ import dataclasses
 class U8Array():
     # Associated value.
     value: bytes
-
-    @staticmethod
-    def from_bytes(as_bytes: bytes) -> "U8Array":
-        raise NotImplementedError()
-
-    @staticmethod
-    def from_json(as_json: str) -> "U8Array":
-        raise NotImplementedError()
-
-    def to_bytes(self) -> bytes:
-        raise NotImplementedError()
-
-    def to_json(self) -> str:
-        raise NotImplementedError()
--- pycspr/types/other/vector.py
+++ pycspr/types/other/vector.py
@@ -8,21 +8,3 @@ from pycspr.types.cl import cl_values
 class Vector():
     # Associated vector.
     vec: typing.List[bytes]
-
-    @staticmethod
-    def from_bytes(as_bytes: bytes) -> "Vector":
-        raise NotImplementedError()
-
-
-    @staticmethod
-    def from_json(as_json: str) -> "Vector":
-        raise NotImplementedError()
-
-
-    def to_bytes(self) -> bytes:
-        return cl_values.CL_U32(len(self.vec)).to_bytes() + \
-               bytes([i for j in self.vec for i in j])
-
-
-    def to_json(self) -> str:
-        raise NotImplementedError()
