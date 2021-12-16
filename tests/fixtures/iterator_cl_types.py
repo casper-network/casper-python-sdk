0db99368108b7a595ad54205133892c3954ff0c5
Mark A. Greenslade <mark@casperlabs.io>
2021-12-16 15:45:07 +0100

    1. Serialisation unit test refactor.

---
+++ tests/fixtures/iterator_cl_types.py
@@ -0,0 +1,108 @@
+import typing
+
+from pycspr import types
+from pycspr.types import CL_Type
+from pycspr.types import CL_TypeKey
+
+
+def yield_cl_types(fixtures: list) -> typing.Iterator[CL_Type]:
+    for fixture in fixtures:
+        type_key = fixture["cl_type"]
+
+        if type_key == CL_TypeKey.ANY:
+            yield types.CL_Type_Any()
+
+        elif type_key == CL_TypeKey.BOOL:
+            yield types.CL_Type_Bool()
+
+        elif type_key == CL_TypeKey.BYTE_ARRAY:
+            yield types.CL_Type_ByteArray(fixture["cl_type_size"])
+
+        elif type_key == CL_TypeKey.I32:
+            yield types.CL_Type_I32()
+
+        elif type_key == CL_TypeKey.I64:
+            yield types.CL_Type_I64()
+
+        elif type_key == CL_TypeKey.KEY:
+            yield types.CL_Type_Key()
+
+        elif type_key == CL_TypeKey.LIST:
+            for inner_type in _get_inner_types():
+                yield types.CL_Type_List(inner_type)
+
+        elif type_key == CL_TypeKey.MAP:
+            yield types.CL_Type_Map(types.CL_Type_String(), types.CL_Type_I32())
+
+        elif type_key == CL_TypeKey.OPTION:
+            for inner_type in _get_inner_types():
+                yield types.CL_Type_Option(inner_type)
+
+        elif type_key == CL_TypeKey.PUBLIC_KEY:
+            yield types.CL_Type_PublicKey()
+
+        elif type_key == CL_TypeKey.RESULT:
+            yield types.CL_Type_Result()
+
+        elif type_key == CL_TypeKey.STRING:
+            yield types.CL_Type_String()
+
+        elif type_key == CL_TypeKey.TUPLE_1:
+            yield types.CL_Type_Tuple1(types.CL_Type_U64())
+
+        elif type_key == CL_TypeKey.TUPLE_2:
+            yield types.CL_Type_Tuple2(types.CL_Type_U64(), types.CL_Type_U128())
+
+        elif type_key == CL_TypeKey.TUPLE_3:
+            yield types.CL_Type_Tuple3(types.CL_Type_U64(), types.CL_Type_U128(), types.CL_Type_U256())
+
+        elif type_key == CL_TypeKey.U8:
+            yield types.CL_Type_U8()
+
+        elif type_key == CL_TypeKey.U32:
+            yield types.CL_Type_U32()
+
+        elif type_key == CL_TypeKey.U64:
+            yield types.CL_Type_U64()
+
+        elif type_key == CL_TypeKey.U128:
+            yield types.CL_Type_U128()
+
+        elif type_key == CL_TypeKey.U256:
+            yield types.CL_Type_U256()
+
+        elif type_key == CL_TypeKey.U512:
+            yield types.CL_Type_U512()
+
+        elif type_key == CL_TypeKey.UNIT:
+            yield types.CL_Type_Unit()
+
+        elif type_key == CL_TypeKey.UREF:
+            yield types.CL_Type_URef()
+
+
+def _get_inner_types():
+    return [
+        types.CL_Type_Any(),
+        types.CL_Type_Bool(),
+        types.CL_Type_ByteArray(32),
+        types.CL_Type_I32(),
+        types.CL_Type_I64(),
+        types.CL_Type_Key(),
+        types.CL_Type_List(types.CL_Type_U64()),
+        types.CL_Type_Map(types.CL_Type_String(), types.CL_Type_I32()),
+        types.CL_Type_PublicKey(),
+        types.CL_Type_Result(),
+        types.CL_Type_String(),
+        types.CL_Type_Tuple1(types.CL_Type_U64()),
+        types.CL_Type_Tuple2(types.CL_Type_U64(), types.CL_Type_U128()),
+        types.CL_Type_Tuple3(types.CL_Type_U64(),types.CL_Type_U128(), types.CL_Type_U256()),
+        types.CL_Type_U8(),
+        types.CL_Type_U32(),
+        types.CL_Type_U64(),
+        types.CL_Type_U128(),
+        types.CL_Type_U256(),
+        types.CL_Type_U512(),
+        types.CL_Type_Unit(),
+        types.CL_Type_URef(),
+    ]
---
+++ tests/fixtures/iterator_cl_values.py
@@ -0,0 +1,91 @@
+import typing
+
+from pycspr import types
+from pycspr.types import CL_TypeKey
+from pycspr.types import CL_Value
+
+
+def yield_cl_values(fixtures: list) -> typing.Iterator[CL_Value]:
+    for fixture in fixtures:
+        type_key = fixture["cl_type"]
+        value = fixture["value"]
+
+        if type_key == CL_TypeKey.ANY:
+            continue
+
+        elif type_key == CL_TypeKey.BOOL:
+            yield types.CL_Bool(value)
+
+        elif type_key == CL_TypeKey.BYTE_ARRAY:
+            yield types.CL_ByteArray(bytes.fromhex(value))
+
+        elif type_key == CL_TypeKey.I32:
+            yield types.CL_I32(value)
+
+        elif type_key == CL_TypeKey.I64:
+            yield types.CL_I64(value)
+
+        elif type_key == CL_TypeKey.KEY:
+            yield types.CL_Key.from_string(value)
+
+        elif type_key == CL_TypeKey.LIST:
+            yield types.CL_List([types.CL_U64(i) for i in value], types.CL_Type_U64())
+
+        elif type_key == CL_TypeKey.MAP:
+            yield types.CL_Map(
+                [(types.CL_String(k), types.CL_U64(v)) for k, v in value.items()]
+            )
+
+        elif type_key == CL_TypeKey.OPTION:
+            yield types.CL_Option(types.CL_U64(value), types.CL_Type_U64())
+
+        elif type_key == CL_TypeKey.PUBLIC_KEY:
+            yield types.CL_PublicKey.from_account_key(bytes.fromhex(value))
+
+        elif type_key == CL_TypeKey.RESULT:
+            continue
+
+        elif type_key == CL_TypeKey.STRING:
+            yield types.CL_String(value)
+
+        elif type_key == CL_TypeKey.TUPLE_1:
+            yield types.CL_Tuple1(
+                types.CL_U64(value[0])
+                )
+
+        elif type_key == CL_TypeKey.TUPLE_2:
+            yield types.CL_Tuple2(
+                types.CL_U64(value[0]),
+                types.CL_U128(value[1])
+                )
+
+        elif type_key == CL_TypeKey.TUPLE_3:
+            yield types.CL_Tuple3(
+                types.CL_U64(value[0]),
+                types.CL_U128(value[1]),
+                types.CL_U256(value[2])
+                )
+
+        elif type_key == CL_TypeKey.U8:
+            yield types.CL_U8(value)
+
+        elif type_key == CL_TypeKey.U32:
+            yield types.CL_U32(value)
+
+        elif type_key == CL_TypeKey.U64:
+            yield types.CL_U64(value)
+
+        elif type_key == CL_TypeKey.U128:
+            yield types.CL_U128(value)
+
+        elif type_key == CL_TypeKey.U256:
+            yield types.CL_U256(value)
+
+        elif type_key == CL_TypeKey.U512:
+            yield types.CL_U512(value)
+
+        elif type_key == CL_TypeKey.UNIT:
+            yield types.CL_Unit()
+
+        elif type_key == CL_TypeKey.UREF:
+            yield types.CL_URef.from_string(value)
