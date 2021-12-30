7cd481d3b56d706ab25251f53df64845fd9caf29
Mark A. Greenslade <mark@casperlabs.io>
2021-12-30 16:51:45 +0100

    1. Further serialisation improvements.

--- pycspr/serialisation/deploy_to_bytes.py
+++ pycspr/serialisation/deploy_to_bytes.py
@@ -23,61 +23,78 @@ def encode(entity: object) -> bytes:
     :returns: An array of bytes.
     
     """
-    if isinstance(entity, Deploy):
-        raise NotImplementedError()
+    try:
+        encoder = _ENCODERS[type(entity)]
+    except KeyError:
+        raise ValueError(f"Unknown deploy type: {entity}")
+    else:
+        return encoder(entity)
 
-    elif isinstance(entity, DeployApproval):
-        return entity.signer + entity.signature
 
-    elif isinstance(entity, DeployArgument):
-        return \
-            cl_value_to_bytes(cl_values.CL_String(entity.name)) + \
-            _u8_array_to_bytes(cl_value_to_bytes(entity.value)) + \
-            cl_type_to_bytes(cl_value_to_cl_type(entity.value))
+def _encode_deploy(entity: Deploy) -> bytes:
+    raise NotImplementedError()
 
-    elif isinstance(entity, DeployHeader):
-        raise NotImplementedError()
 
-    elif isinstance(entity, ModuleBytes):
-        return \
-            bytes([0]) + \
-            _u8_array_to_bytes(list(entity.module_bytes)) + \
-            _vector_to_bytes(list(map(encode, entity.arguments)))
+def _encode_deploy_approval(entity: DeployApproval) -> bytes:
+    return entity.signer + entity.signature
 
-    elif isinstance(entity, StoredContractByHash):
-        return \
-            bytes([1]) + \
-            cl_value_to_bytes(cl_values.CL_ByteArray(entity.hash)) + \
-            cl_value_to_bytes(cl_values.CL_String(entity.entry_point)) + \
-            _vector_to_bytes(list(map(encode, entity.arguments)))
 
-    elif isinstance(entity, StoredContractByHashVersioned):
-        return \
-            bytes([2]) + \
-            cl_value_to_bytes(cl_values.CL_ByteArray(entity.hash)) + \
-            cl_value_to_bytes(cl_values.CL_U32(entity.version)) + \
-            cl_value_to_bytes(cl_values.CL_String(entity.entry_point)) + \
-            _vector_to_bytes(list(map(encode, entity.arguments)))
+def _encode_deploy_argument(entity: DeployArgument) -> bytes:
+    return \
+        cl_value_to_bytes(cl_values.CL_String(entity.name)) + \
+        _u8_array_to_bytes(cl_value_to_bytes(entity.value)) + \
+        cl_type_to_bytes(cl_value_to_cl_type(entity.value))
 
-    elif isinstance(entity, StoredContractByName):
-        return \
-            bytes([3]) + \
-            cl_value_to_bytes(cl_values.CL_String(entity.name)) + \
-            cl_value_to_bytes(cl_values.CL_String(entity.entry_point)) + \
-            _vector_to_bytes(list(map(encode, entity.arguments)))
 
-    elif isinstance(entity, StoredContractByNameVersioned):
-        return \
-            bytes([4]) + \
-            cl_value_to_bytes(cl_values.CL_String(entity.name)) + \
-            cl_value_to_bytes(cl_values.CL_U32(entity.version)) + \
-            cl_value_to_bytes(cl_values.CL_String(entity.entry_point)) + \
-            _vector_to_bytes(list(map(encode, entity.arguments)))
+def _encode_deploy_header(entity: DeployHeader) -> bytes:
+    raise NotImplementedError()
 
-    elif isinstance(entity, Transfer):
-        return \
-            bytes([5]) + \
-            _vector_to_bytes(list(map(encode, entity.arguments)))
+
+def _encode_module_bytes(entity: ModuleBytes) -> bytes:
+    return \
+        bytes([0]) + \
+        _u8_array_to_bytes(list(entity.module_bytes)) + \
+        _vector_to_bytes(list(map(encode, entity.arguments)))
+
+
+def _encode_stored_contract_by_hash(entity: StoredContractByHash) -> bytes:
+    return \
+        bytes([1]) + \
+        cl_value_to_bytes(cl_values.CL_ByteArray(entity.hash)) + \
+        cl_value_to_bytes(cl_values.CL_String(entity.entry_point)) + \
+        _vector_to_bytes(list(map(encode, entity.arguments)))
+
+
+def _encode_stored_contract_by_hash_versioned(entity: StoredContractByHashVersioned) -> bytes:
+    return \
+        bytes([2]) + \
+        cl_value_to_bytes(cl_values.CL_ByteArray(entity.hash)) + \
+        cl_value_to_bytes(cl_values.CL_U32(entity.version)) + \
+        cl_value_to_bytes(cl_values.CL_String(entity.entry_point)) + \
+        _vector_to_bytes(list(map(encode, entity.arguments)))
+
+
+def _encode_stored_contract_by_name(entity: StoredContractByName) -> bytes:
+    return \
+        bytes([3]) + \
+        cl_value_to_bytes(cl_values.CL_String(entity.name)) + \
+        cl_value_to_bytes(cl_values.CL_String(entity.entry_point)) + \
+        _vector_to_bytes(list(map(encode, entity.arguments)))
+
+
+def _encode_stored_contract_by_name_versioned(entity: StoredContractByNameVersioned) -> bytes:
+    return \
+        bytes([4]) + \
+        cl_value_to_bytes(cl_values.CL_String(entity.name)) + \
+        cl_value_to_bytes(cl_values.CL_U32(entity.version)) + \
+        cl_value_to_bytes(cl_values.CL_String(entity.entry_point)) + \
+        _vector_to_bytes(list(map(encode, entity.arguments)))
+
+
+def _encode_transfer(entity: Transfer) -> bytes:
+    return \
+        bytes([5]) + \
+        _vector_to_bytes(list(map(encode, entity.arguments)))
 
 
 def _u8_array_to_bytes(value: typing.List[int]) -> bytes:
@@ -88,3 +105,17 @@ def _vector_to_bytes(value: typing.List) -> bytes:
     return \
         cl_value_to_bytes(cl_values.CL_U32(len(value))) + \
         bytes([i for j in value for i in j])
+
+
+_ENCODERS = {
+    Deploy: _encode_deploy,
+    DeployApproval: _encode_deploy_approval,
+    DeployArgument: _encode_deploy_argument,
+    DeployHeader: _encode_deploy_header,
+    ModuleBytes: _encode_module_bytes,
+    StoredContractByHash: _encode_stored_contract_by_hash,
+    StoredContractByHashVersioned: _encode_stored_contract_by_hash_versioned,
+    StoredContractByName: _encode_stored_contract_by_name,
+    StoredContractByNameVersioned: _encode_stored_contract_by_name_versioned,
+    Transfer: _encode_transfer,
+}
--- pycspr/serialisation/deploy_to_json.py
+++ pycspr/serialisation/deploy_to_json.py
@@ -19,87 +19,118 @@ def encode(entity: object) -> dict:
     :returns: A JSON compatible dictionary.
     
     """
-    if isinstance(entity, Deploy):
-        return {
-            "approvals": [encode(i) for i in entity.approvals],
-            "hash": cl_checksum.encode_digest(entity.hash),
-            "header": encode(entity.header),
-            "payment": encode(entity.payment),
-            "session": encode(entity.session)
-        }
+    try:
+        encoder = _ENCODERS[type(entity)]
+    except KeyError:
+        raise ValueError(f"Unknown deploy type: {entity}")
+    else:
+        return encoder(entity)    
 
-    elif isinstance(entity, DeployApproval):
-        return {
-            "signature": cl_checksum.encode_signature(entity.signature),
-            "signer": cl_checksum.encode_account_key(entity.signer.account_key)
-        }
 
-    elif isinstance(entity, DeployArgument):
-        return [
-            entity.name,
-            cl_value_to_json(entity.value)
-        ]
+def _encode_deploy(entity: Deploy) -> dict:
+    return {
+        "approvals": [encode(i) for i in entity.approvals],
+        "hash": cl_checksum.encode_digest(entity.hash),
+        "header": encode(entity.header),
+        "payment": encode(entity.payment),
+        "session": encode(entity.session)
+    }
 
-    elif isinstance(entity, DeployHeader):
-        return {
-            "account": cl_checksum.encode_account_key(entity.account_public_key.account_key),
-            "body_hash": cl_checksum.encode_digest(entity.body_hash),
-            "chain_name": entity.chain_name,
-            "dependencies": entity.dependencies,
-            "gas_price": entity.gas_price,
-            "timestamp": entity.timestamp.to_string(),
-            "ttl": entity.ttl.to_string()
-        }
 
-    elif isinstance(entity, ModuleBytes):
-        return {
-            "ModuleBytes": {
-                "args": [encode(i) for i in entity.arguments],
-                "module_bytes": cl_checksum.encode(entity.module_bytes)
-            }
-        }
+def _encode_deploy_approval(entity: DeployApproval) -> dict:
+    return {
+        "signature": cl_checksum.encode_signature(entity.signature),
+        "signer": cl_checksum.encode_account_key(entity.signer.account_key)
+    }
 
-    elif isinstance(entity, StoredContractByHash):
-        return {
-            "StoredContractByHash": {
-                "args": [encode(i) for i in entity.arguments],
-                "entry_point": entity.entry_point,
-                "hash": cl_checksum.encode(entity.hash)
-            }
-        }
 
-    elif isinstance(entity, StoredContractByHashVersioned):
-        return {
-            "StoredContractByHashVersioned": {
-                "args": [encode(i) for i in entity.arguments],
-                "entry_point": entity.entry_point,
-                "hash": cl_checksum.encode(entity.hash),
-                "version": entity.version
-            }
-        }
+def _encode_deploy_argument(entity: DeployArgument) -> dict:
+    return [
+        entity.name,
+        cl_value_to_json(entity.value)
+    ]
 
-    elif isinstance(entity, StoredContractByName):
-        return {
-            "StoredContractByName": {
-                "args": [encode(i) for i in entity.arguments],
-                "entry_point": entity.entry_point,
-                "name": encode.name
-            }
-        }
 
-    elif isinstance(entity, StoredContractByNameVersioned):
-        return {
-            "StoredContractByNameVersioned": {
-                "args": [encode(i) for i in entity.arguments],
-                "entry_point": entity.entry_point,
-                "name": encode.name,
-                "version": encode.version
-            }
-        }
+def _encode_deploy_header(entity: DeployHeader) -> dict:
+    return {
+        "account": cl_checksum.encode_account_key(entity.account_public_key.account_key),
+        "body_hash": cl_checksum.encode_digest(entity.body_hash),
+        "chain_name": entity.chain_name,
+        "dependencies": entity.dependencies,
+        "gas_price": entity.gas_price,
+        "timestamp": entity.timestamp.to_string(),
+        "ttl": entity.ttl.to_string()
+    }
 
-    elif isinstance(entity, Transfer):
-        return {
-            "Transfer": {
-                "args": [encode(i) for i in entity.arguments],
-            }
+
+def _encode_module_bytes(entity: ModuleBytes) -> dict:
+    return {
+        "ModuleBytes": {
+            "args": [encode(i) for i in entity.arguments],
+            "module_bytes": cl_checksum.encode(entity.module_bytes)
         }
+    }
+
+
+def _encode_stored_contract_by_hash(entity: StoredContractByHash) -> dict:
+    return {
+        "StoredContractByHash": {
+            "args": [encode(i) for i in entity.arguments],
+            "entry_point": entity.entry_point,
+            "hash": cl_checksum.encode(entity.hash)
+        }
+    }
+
+
+def _encode_stored_contract_by_hash_versioned(entity: StoredContractByHashVersioned) -> dict:
+    return {
+        "StoredContractByHashVersioned": {
+            "args": [encode(i) for i in entity.arguments],
+            "entry_point": entity.entry_point,
+            "hash": cl_checksum.encode(entity.hash),
+            "version": entity.version
+        }
+    }
+
+
+def _encode_stored_contract_by_name(entity: StoredContractByName) -> dict:
+    return {
+        "StoredContractByName": {
+            "args": [encode(i) for i in entity.arguments],
+            "entry_point": entity.entry_point,
+            "name": encode.name
+        }
+    }
+
+
+def _encode_stored_contract_by_name_versioned(entity: StoredContractByNameVersioned) -> dict:
+    return {
+        "StoredContractByNameVersioned": {
+            "args": [encode(i) for i in entity.arguments],
+            "entry_point": entity.entry_point,
+            "name": encode.name,
+            "version": encode.version
+        }
+    }
+
+
+def _encode_transfer(entity: Transfer) -> dict:
+    return {
+        "Transfer": {
+            "args": [encode(i) for i in entity.arguments],
+        }
+    }
+
+
+_ENCODERS = {
+    Deploy: _encode_deploy,
+    DeployApproval: _encode_deploy_approval,
+    DeployArgument: _encode_deploy_argument,
+    DeployHeader: _encode_deploy_header,
+    ModuleBytes: _encode_module_bytes,
+    StoredContractByHash: _encode_stored_contract_by_hash,
+    StoredContractByHashVersioned: _encode_stored_contract_by_hash_versioned,
+    StoredContractByName: _encode_stored_contract_by_name,
+    StoredContractByNameVersioned: _encode_stored_contract_by_name_versioned,
+    Transfer: _encode_transfer,
+}
