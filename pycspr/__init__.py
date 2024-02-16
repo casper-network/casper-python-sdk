b4baffc552b7dc6cc220270bedf386759e968b17
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-02-16 01:19:40 +0100

    1. Version bump.

--- pycspr/__init__.py
+++ pycspr/__init__.py
@@ -7,7 +7,7 @@
 #  88              d8'                              88
 
 __title__ = "pycspr"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "Mark A. Greenslade et al"
 __license__ = "Apache 2.0"
 
