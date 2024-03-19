85fdde90a2193c2d5850d110ac89f37e53b343cd
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-03-19 21:26:04 +0100

    1. Removed litmus sub-package as this is now moved to a dedicated repo.

--- pycspr/litmus/events.py
+++
@@ -1,7 +0,0 @@
-from pycspr.litmus.types import BlockHash
-# from pycspr.litmus.types import Kernel
-
-
-def on_block_finalisation(block_id: BlockHash):
-    print(block_id)
-
--- pycspr/litmus/factory.py
+++
@@ -1,6 +0,0 @@
-from pycspr.litmus.kernel import Kernel
-from pycspr.litmus.types import BlockHash
-
-
-def create_kernel(block_id: BlockHash):
-    return Kernel(block_id)
