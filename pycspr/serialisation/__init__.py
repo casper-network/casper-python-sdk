4d71867ef486feb927c8a2009cd7e145adfaebc9
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-03-19 21:37:18 +0100

    1. Removed experimental tests.

--- pycspr/serialisation/__init__.py
+++ pycspr/serialisation/__init__.py
@@ -3,10 +3,12 @@ from pycspr.serialisation.binary import from_bytes
 from pycspr.serialisation.json import to_json
 from pycspr.serialisation.json import from_json
 from pycspr.serialisation.utils.cl_value_to_cl_type import encode as cl_value_to_cl_type
+from pycspr.serialisation.utils.cl_value_to_parsed import encode as cl_value_to_parsed
 
 
 __all__ = [
     "cl_value_to_cl_type",
+    "cl_value_to_parsed",
     "to_bytes",
     "to_json",
     "from_bytes",
