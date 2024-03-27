942fd0d8b6661599df26ba3cb7a1a8124ce77c76
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-03-27 10:53:36 +0100

    1. Optimising type conversion.

--- tests/test_utils_conversion.py
+++ tests/test_utils_conversion.py
@@ -1,14 +1,13 @@
 import random
 
-import pycspr
+from pycspr.utils import convertor
 
 
 def test_that_humanized_time_interval_can_be_converted_to_milliseconds():
-    convertor = pycspr.utils.conversion.humanized_time_interval_to_ms
     for (unit, ms, quantity) in (
         ("ms", 1, random.randint(1, int(1e9))),
         ("s", 1000, random.randint(1, 60)),
         ("m", 60000, random.randint(1, 60)),
         ("h", 3600000, random.randint(1, 2)),
     ):
-        assert convertor(f"{quantity}{unit}") == quantity * ms
+        assert convertor.ms_from_humanized_time_interval(f"{quantity}{unit}") == quantity * ms
