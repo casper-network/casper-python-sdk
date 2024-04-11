0da3d637656996033c0309e623fca0e8ec2ca844
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-04-11 15:50:41 +0300

    1. Version bump.

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -118,7 +118,7 @@ _INTERFACE_OF_LIBRARY = {
 
 
 def test_version_of_library():
-    assert pycspr.__version__ == "1.2.0"
+    assert pycspr.__version__ == "1.2.1"
 
 
 def test_exports_of_library():
