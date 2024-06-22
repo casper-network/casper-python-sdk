7ee30fa3de14c4369455bf7f2208221db482dbb4
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-06-22 19:12:18 +0200

    1. Refinement of node REST api support.

--- tests/test_api_rest.py
+++ tests/test_api_rest.py
@@ -1,3 +1,5 @@
+import typing
+
 from pycspr.api.node.rest import Client
 from pycspr.api.node.rest.types import NodeStatus
 
@@ -11,6 +13,27 @@ async def test_get_chainspec(NODE_REST_CLIENT: Client):
     assert "chainspec_bytes" in data
     assert "maybe_genesis_accounts_bytes" in data
     assert "maybe_global_state_bytes" in data
+
+
+async def test_get_chain_heights_1(NODE_REST_CLIENT: Client):
+    heights: typing.Tuple[int, int] = await NODE_REST_CLIENT.ext.get_chain_heights()
+    assert isinstance(heights[0], int)
+    assert heights[0] > 0
+    assert isinstance(heights[1], int)
+    assert heights[1] > 0
+    assert heights[1] > heights[0]
+
+
+async def test_get_chain_heights_2(NODE_REST_CLIENT: Client):
+    height: int = await NODE_REST_CLIENT.ext.get_era_height()
+    assert isinstance(height, int)
+    assert height > 0
+
+
+async def test_get_chain_heights_3(NODE_REST_CLIENT: Client):
+    height: int = await NODE_REST_CLIENT.ext.get_block_height()
+    assert isinstance(height, int)
+    assert height > 0
 
 
 async def test_get_node_metrics(NODE_REST_CLIENT: Client):
