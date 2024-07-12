ceb7750519b0b8c1c8c4ec847b131a3b726fa4cb
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-07-12 11:37:42 +0200

    1. Binary port server refactoring.

---
+++ pycspr/api/node/bin/codec/decoder/requests/__init__.py
@@ -0,0 +1,7 @@
+import typing
+
+from pycspr.api.node.bin.codec.decoder.requests.core import \
+    DECODERS as _DECODERS_1
+
+# Set of decoders within current scope.
+DECODERS: typing.Dict[typing.Type, typing.Callable] = _DECODERS_1
