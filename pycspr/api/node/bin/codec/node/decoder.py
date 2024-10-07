6ae83de219b928f393530d6d2cf8c2b90fee8924
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-10-07 13:09:54 +0200

    1. Node binary port: node last progress.

--- pycspr/api/node/bin/codec/node/decoder.py
+++ pycspr/api/node/bin/codec/node/decoder.py
@@ -4,9 +4,11 @@ from pycspr.api.node.bin.codec.utils import decode, register_decoders
 from pycspr.api.node.bin.types.node import \
     NodeAddress, \
     NodeId, \
+    NodeLastProgress, \
     NodePeerEntry, \
     NodeUptime
 from pycspr.api.node.bin.types.primitives.numeric import U64
+from pycspr.api.node.bin.types.primitives.time import Timestamp
 
 
 def _decode_node_peer_entry(bytes_in: bytes) -> typing.Tuple[bytes, NodePeerEntry]:
@@ -20,5 +22,6 @@ register_decoders({
     (NodePeerEntry, _decode_node_peer_entry),
     (NodeAddress, lambda x: decode(str, x)),
     (NodeId, lambda x: decode(str, x)),
+    (NodeLastProgress, lambda x: decode(Timestamp, x)),
     (NodeUptime, lambda x: decode(U64, x)),
 })
--- tests/test_api_node_bin_3.py
+++ tests/test_api_node_bin_3.py
@@ -1,32 +1,33 @@
 import typing
 
 from pycspr.api.node.bin import Client
-from pycspr.api.node.bin.types.node import NodePeerEntry, NodeUptime
+from pycspr.api.node.bin.types.node import NodePeerEntry, NodeUptime, NodeLastProgress
+from pycspr.api.node.bin.types.primitives.time import Timestamp
 
 
 async def test_get_information_node_last_progress(NODE_BINARY_CLIENT: Client, REQUEST_ID: int):
     data = \
         await NODE_BINARY_CLIENT.get_information_node_last_progress(request_id=REQUEST_ID)
+    assert isinstance(data, Timestamp)
+
+
+async def test_get_information_node_peers(NODE_BINARY_CLIENT: Client, REQUEST_ID: int):
+    data: typing.List[NodePeerEntry] = \
+        await NODE_BINARY_CLIENT.get_information_node_peers(request_id=REQUEST_ID)
+    assert isinstance(data, list)
+    for entity in data:
+        assert isinstance(entity, NodePeerEntry)
+        assert isinstance(entity.address, str)
+        assert isinstance(entity.node_id, str)
+
+
+async def test_get_information_node_reactor_state(NODE_BINARY_CLIENT: Client, REQUEST_ID: int):
+    data = \
+        await NODE_BINARY_CLIENT.get_information_node_reactor_state(request_id=REQUEST_ID)
+    assert isinstance(data, str)
+
+
+async def test_get_information_node_uptime(NODE_BINARY_CLIENT: Client, REQUEST_ID: int):
+    data: NodeUptime = \
+        await NODE_BINARY_CLIENT.get_information_node_uptime(request_id=REQUEST_ID)
     assert isinstance(data, int)
-
-
-# async def test_get_information_node_peers(NODE_BINARY_CLIENT: Client, REQUEST_ID: int):
-#     data: typing.List[NodePeerEntry] = \
-#         await NODE_BINARY_CLIENT.get_information_node_peers(request_id=REQUEST_ID)
-#     assert isinstance(data, list)
-#     for entity in data:
-#         assert isinstance(entity, NodePeerEntry)
-#         assert isinstance(entity.address, str)
-#         assert isinstance(entity.node_id, str)
-
-
-# async def test_get_information_node_reactor_state(NODE_BINARY_CLIENT: Client, REQUEST_ID: int):
-#     data = \
-#         await NODE_BINARY_CLIENT.get_information_node_reactor_state(request_id=REQUEST_ID)
-#     assert isinstance(data, str)
-
-
-# async def test_get_information_node_uptime(NODE_BINARY_CLIENT: Client, REQUEST_ID: int):
-#     data: NodeUptime = \
-#         await NODE_BINARY_CLIENT.get_information_node_uptime(request_id=REQUEST_ID)
-#     assert isinstance(data, int)
