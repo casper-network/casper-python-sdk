c8eeb91900ac1f3bbcfa02f349afedc4587943c6
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-10-16 12:17:23 +0200

    1. Major refactor of type system.

--- pycspr/api/node/bin/types/__init__.py
+++ pycspr/api/node/bin/types/__init__.py
@@ -1,5 +1,4 @@
 from pycspr.api.node.bin.types import chain
-from pycspr.api.node.bin.types import crypto
 from pycspr.api.node.bin.types import node
 from pycspr.api.node.bin.types import primitives
 from pycspr.api.node.bin.types import transport
